28eddc20a61a31535d39050211df8b114d5b46a8
cuebitt <136763482+cuebitt@users.noreply.github.com>
2024-11-13 15:22:26 -0800

    Add integrations
    Syntax highlighting, embeds, opengraph tags, sitemap. Also add favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,17 +8,26 @@ import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 // @ts-ignore
 import sectionize from "@hbsnow/rehype-sectionize";
 import alpineIntersectHeading from "./src/util/rehype-alpine-intersect";
+import sitemap from "@astrojs/sitemap";
+import expressiveCode from "astro-expressive-code";
+import opengraphImages, { presets } from "astro-opengraph-images";
+import { readFileSync } from "fs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    icon(),
-    alpinejs({ entrypoint: "/src/util/alpine-entrypoint" }),
-    mdx(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), icon(), alpinejs({ entrypoint: "/src/util/alpine-entrypoint" }), expressiveCode(), mdx(), sitemap(), opengraphImages({
+    options: {
+      fonts: [{
+        name: "Montserrat",
+        weight: 400,
+        style: "normal",
+        data: readFileSync("node_modules/@fontsource/montserrat/files/montserrat-latin-400-normal.woff2"),
+      }]
+    },
+    render: presets.blackAndWhite
+  })],
   image: {
     remotePatterns: [{ protocol: "https" }],
   },
