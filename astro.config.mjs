3092dab48d9cbee08a6fc780076adf92325c50dd
cuebitt <136763482+cuebitt@users.noreply.github.com>
2024-11-13 16:05:45 -0800

    Clean up
    Manually insert OpenGraph images
    Remove unused dependencies
    Fix favicon type
    Ensure ref sheet is always PNG for artists
    Replace Lucide icons with FA Solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,24 +10,19 @@ import sectionize from "@hbsnow/rehype-sectionize";
 import alpineIntersectHeading from "./src/util/rehype-alpine-intersect";
 import sitemap from "@astrojs/sitemap";
 import expressiveCode from "astro-expressive-code";
-import opengraphImages, { presets } from "astro-opengraph-images";
-import { readFileSync } from "fs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), icon(), alpinejs({ entrypoint: "/src/util/alpine-entrypoint" }), expressiveCode(), mdx(), sitemap(), opengraphImages({
-    options: {
-      fonts: [{
-        name: "Montserrat",
-        weight: 400,
-        style: "normal",
-        data: readFileSync("node_modules/@fontsource/montserrat/files/montserrat-latin-400-normal.woff2"),
-      }]
-    },
-    render: presets.blackAndWhite
-  })],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+    alpinejs({ entrypoint: "/src/util/alpine-entrypoint" }),
+    expressiveCode(),
+    mdx(),
+    sitemap(),
+    ],
   image: {
     remotePatterns: [{ protocol: "https" }],
   },
