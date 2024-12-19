5975434e14f5298dbfdb8beee7faebd345522877
cuebitt <136763482+cuebitt@users.noreply.github.com>
2024-12-18 18:34:23 -0800

    Replace Tailwind with UnoCSS

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,9 @@
 import eslintPluginAstro from "eslint-plugin-astro";
-import tailwind from "eslint-plugin-tailwindcss";
+import unocss from "@unocss/eslint-config/flat";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
   ...eslintPluginAstro.configs["jsx-a11y-recommended"],
-  {
-    rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
-    },
-  },
-
-  ...tailwind.configs["flat/recommended"],
+  unocss,
+  eslintConfigPrettier,
 ];
