b1309a25ff08ae30db5c127fb1bbdeb45dc36dff
cuebitt <136763482+cuebitt@users.noreply.github.com>
2025-02-19 12:33:04 -0800

    Create ColorSwatches component

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,22 +8,13 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'My Docs',
-
+      components: {
+        Head: "./src/components/Head.astro",
+      },
       sidebar: [
         {
           label: "Characters",
           autogenerate: { directory: 'characters' },
-        },
-        {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', slug: 'guides/example' },
-          ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
         },
       ],
       customCss: [
