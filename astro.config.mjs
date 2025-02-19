47ab27755355fc928317ea42c01971cb57c3248f
cuebitt <136763482+cuebitt@users.noreply.github.com>
2025-02-19 13:36:12 -0800

    Create AttributedImage, Carousel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ['lh3.googleusercontent.com']
+  },
   integrations: [
     starlight({
       title: 'My Docs',
