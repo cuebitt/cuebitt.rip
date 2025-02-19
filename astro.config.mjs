7f402de74603371979202895255f129afe6a56dd
cuebitt <136763482+cuebitt@users.noreply.github.com>
2025-02-19 15:02:13 -0800

    Add .editorconfig, auto-format

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,33 +6,33 @@ import starlightObsidian, { obsidianSidebarGroup } from "starlight-obsidian";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    domains: ["lh3.googleusercontent.com"],
-  },
-  integrations: [
-    starlight({
-      title: "😇 Cuebitt's Notebook",
-      favicon: "/favicon.ico",
-      components: {
-        Head: "./src/components/Head.astro",
-      },
-      sidebar: [
-        {
-          label: "Characters",
-          autogenerate: { directory: "characters" },
-        },
-        obsidianSidebarGroup,
-      ],
-      customCss: ["./src/styles/global.css"],
-      plugins: [
-        starlightObsidian({
-          vault: "./cuebitt.rip",
-        }),
-      ],
-    }),
-  ],
+	image: {
+		domains: ["lh3.googleusercontent.com"],
+	},
+	integrations: [
+		starlight({
+			title: "😇 Cuebitt's Notebook",
+			favicon: "/favicon.ico",
+			components: {
+				Head: "./src/components/Head.astro",
+			},
+			sidebar: [
+				{
+					label: "Characters",
+					autogenerate: { directory: "characters" },
+				},
+				obsidianSidebarGroup,
+			],
+			customCss: ["./src/styles/global.css"],
+			plugins: [
+				starlightObsidian({
+					vault: "./cuebitt.rip",
+				}),
+			],
+		}),
+	],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,6 @@ import eslintPluginAstro from "eslint-plugin-astro";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
-  eslintConfigPrettier,
+	...eslintPluginAstro.configs["jsx-a11y-recommended"],
+	eslintConfigPrettier,
 ];
